afedcb22535739ebbc21d945de631768476d763f
Natalya <m_d.r.lecter@protonmail.com>
2022-02-24 19:45:56 +0300

    Homework 4

--- src/components/MessageList/MessageList.jsx
+++ src/components/MessageList/MessageList.jsx
@@ -5,7 +5,7 @@ export const MessageList = ({ messages }) => (
     <p>Think about the question you're conserned</p>
     <ul>
       {messages.map((message) => (
-        <li key={message.id}>
+        <li className="test" key={message.id}>
           {message.author}: {message.text}
         </li>
       ))}
